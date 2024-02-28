43a6b5ba7a8b2dec3216753a31b001888e538add
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-28 18:56:28 -0400

    Adding files fixed on root

--- models/__init__.py
+++ models/__init__.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-"""Module __init__"""
-from models.engine.file_storage import FileStorage
 
+from models.engine.file_storage import FileStorage
 
 storage = FileStorage()
 storage.reload()
