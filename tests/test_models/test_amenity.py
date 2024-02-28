e509396eaa653f40ac42b8164495a325f4ccc745
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-28 18:11:59 -0400

    Fixing unittest deletion extra code

--- tests/test_models/test_amenity.py
+++ tests/test_models/test_amenity.py
@@ -1,42 +1,16 @@
 #!/usr/bin/python3
-"""Module test_amenity"""
+"""Test for amenity"""
+
 import unittest
 from models.amenity import Amenity
-from models.base_model import BaseModel
 
 
-class TestAmenity(unittest.TestCase):
-    """Test for the Amenity class"""
+class test_camenity(unittest.TestCase):
+    """tests amenity"""
 
-    def setup(self):
-        """Set up"""
-        self.a1 = Amenity()
+    def test_amenity(self):
+        am = Amenity()
+        self.assertEqual(am.name, "")
 
-    def test_name(self):
-        """Testing Amenity name"""
-        self.assertEqual(self.amenity.name, "")
-
-    def test_inheritance(self):
-        """Testing Amenity inheritance"""
-        self.assertIsInstance(self.amenity, BaseModel)
-
-    def test_attributes(self):
-        """Testing Amenity attributes"""
-        self.assertTrue(hasattr(self.amenity, "name"))
-        self.assertTrue(hasattr(self.amenity, "id"))
-        self.assertTrue(hasattr(self.amenity, "created_at"))
-        self.assertTrue(hasattr(self.amenity, "updated_at"))
-
-    def test_attribute_default(self):
-        """Test attribute default"""
-        self.assertEqual(self.amenity.name, "")
-
-    def test_str(self):
-        """Testing Amenity __str__"""
-        expected = "[Amenity] ({}) {}"
-        expected = expected.format(self.amenity.id, self.amenity.__dict__)
-
-        self.assertEqual(expected, str(self.amenity))
-
-    if __name__ == "__main__":
+if __name__ == '__main__':
         unittest.main()
