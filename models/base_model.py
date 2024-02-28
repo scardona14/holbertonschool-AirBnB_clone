45d08f3a81ea80ad5419d5800693522cd257b7c9
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-28 14:14:11 -0400

    Base model fix

--- models/base_model.py
+++ models/base_model.py
@@ -6,20 +6,21 @@ import models
 
 
 class BaseModel:
+   class BaseModel:
     """Base class for AirBnB clone"""
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
-        if kwargs is not None and len(kwargs) > 0:
+        self.id = str(uuid.uuid4())
+        self.created_at = datetime.datetime.now()
+        self.updated_at = datetime.datetime.now()
+
+        if kwargs:
             for key, value in kwargs.items():
-                if key == "created_at" or key == "updated_at":
+                if key in ["created_at", "updated_at"]:
                     value = datetime.datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                if key != "__class__":
-                    setattr(self, key, value)
-        else:
-            self.id = str(uuid.uuid4())
-            self.created_at = datetime.datetime.now()
-            self.updated_at = datetime.datetime.now()
-            models.storage.new(self)
+                setattr(self, key, value)
+
+        models.storage.new(self)
 
     def __str__(self):
         """Function that returns official string rep of instances"""
