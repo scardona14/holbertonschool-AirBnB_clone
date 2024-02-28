e50aa02699eade5dfff6a551f3e001d1231c5d5e
Pedro Augusto Laguna <lagunaaustore@gmail.com>
2024-02-28 19:31:07 -0400

    Adding files fixed on root

--- models/base_model.py
+++ models/base_model.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-#!/usr/bin/python3
 """This module contains the BaseModel class for the AirBnB clone"""
-import uuid
+from uuid import uuid4
 from datetime import datetime
 import models
 
@@ -11,7 +10,7 @@ class BaseModel:
     """Base class for AirBnB clone"""
     def __init__(self, *args, **kwargs):
         """Initialization of the base model"""
-        self.id = str(uuid.uuid4())
+        self.id = str(uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
 
--- tests/test_models/test_engine/test_file_storage.py
+++ tests/test_models/test_engine/test_file_storage.py
@@ -1,3 +1,4 @@
+#!/usr/bin/python3
 import unittest
 from models.engine.file_storage import FileStorage
 import json
